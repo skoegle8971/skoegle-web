7271ee873843a72d2e938719a8abd3c483697863
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-04-09 09:36:43 +0530

    sdf

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,15 @@
 import Layout from '../Layout/Layout'
-
-
+import IotBlogs from '../Components/IotBlogs'
+import  IotProjects from '../Components/IotProjects'
+import IotServices from '../Components/IotServices'
+import IotTech from '../Components/IotTech'
 export default function App(){
   return(
    <Layout>
-    <h1>Skoegle</h1>
+  <IotBlogs/>
+  <IotServices/>
+  <IotProjects/>
+  <IotTech/>
     
     </Layout>
   )
