fc218ae3094bee09b318cf05f26b50641614fd6e
Akriti786 <akriti786shukla@gmail.com>
2025-05-13 15:40:32 +0530

    mrbsvjfj

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,3 +1,11 @@
+// app/home/home.jsx
 "use client";
-
 import Layout from "../../Layout/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>Hello from client</div>
+    </Layout>
+  );
+}
