195042b5b24c9dd47bc0b79a38f1e9da15a35545
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-06-26 16:33:00 +0530

    dfhd

--- src/app/pages/products/view/[productId]/page.jsx
+++ src/app/pages/products/view/[productId]/page.jsx
@@ -2,7 +2,7 @@
 
 import Layout from '@/Components/Layout/Layout';
 import { useParams } from 'next/navigation';
-import Readmore from '@/components/Pages/Product/Readmore';
+import Readmore from '@/Components/Pages/Product/Readmore';
 
 export default function ProductPage() {
   const { productId } = useParams();
